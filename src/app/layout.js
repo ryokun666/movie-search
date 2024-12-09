19615fe26a8e208a4cc5ac34d6e67efcfa2fb9ee
ryomen666 <113868184+ryomen666@users.noreply.github.com>
2024-12-09 18:14:19 +0900

    feat: ファビコンを設定する。

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="#映画メモ" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
